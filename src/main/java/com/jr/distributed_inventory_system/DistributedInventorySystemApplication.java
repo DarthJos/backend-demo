88223170eb83772d10bfcdb51fa9994f45dd4eab
Josimar Reyes C <darth_jos@hotmail.com>
2025-10-22 00:54:11 -0600

    Initial commit: Project structure and documentation directories.

---
+++ src/main/java/com/jr/distributed_inventory_system/DistributedInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.jr.distributed_inventory_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedInventorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jr/distributed_inventory_system/DistributedInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jr.distributed_inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
