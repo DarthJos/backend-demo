263c5dea199b99c4bca7369b32f95f89b4018733
Josimar Reyes C <darth_jos@hotmail.com>
2025-10-23 23:58:39 -0600

    Fase 2 | Tarea 2.2 | Lógica de negocio y consistencia

--- src/main/java/com/jr/distributed_inventory_system/repository/InventoryRepository.java
+++ src/main/java/com/jr/distributed_inventory_system/repository/InventoryRepository.java
@@ -13,7 +13,7 @@ public interface InventoryRepository extends JpaRepository<InventoryItem, String
 
     /**
      * Busca un InventoryItem por ProductId y StoreId.
-     * * @param productId ID del producto (SKU)
+     * @param productId ID del producto (SKU)
      * @param storeId ID de la tienda
      * @return Un Optional que contiene el InventoryItem o vacío si no existe.
      */
@@ -24,7 +24,7 @@ public interface InventoryRepository extends JpaRepository<InventoryItem, String
      * Esto asegura que ningún otro proceso pueda leer/escribir este registro
      * hasta que la transacción actual termine (commit o rollback), garantizando
      * la Consistencia Fuerte.
-     * * @param productId ID del producto
+     * @param productId ID del producto
      * @param storeId ID de la tienda
      * @return El InventoryItem bloqueado.
      */
